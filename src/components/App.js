d5cd971dd19a583a50e1f1925c3bf7829fc9ebb8
Carl <carlbrainegebe@gmail.com>
2021-06-29 01:55:07 -0500

    commit tree d573efc83934cc1b1991a2afa729ef9846afc909

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useEffect, useState} from "react"
+
+function App() {
+ const [randomizedImage, setRandomizedImage] = useState(null)
+ const [fetchComplete, setFetchComplete] = useState(false);
+    useEffect(  ()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then( (response)=>response.json() )
+        .then( (responseData)=>{
+            setRandomizedImage(responseData.message);
+            setFetchComplete(true);
+        } )
+    }  , []);
+
+    if(!fetchComplete) {
+        return(<p>Loading...</p>);
+    }
+
+    return(
+        <>
+        <img src={randomizedImage} alt="A Random Dog"/>
+        </>
+    );
+
+
+}
+
+export default App;
